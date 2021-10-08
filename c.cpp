ffbfde7eb252e3803c62d675656538a6f36ef35e
Ayush Kumar Singh <85936494+Ayush-J2002@users.noreply.github.com>
2021-10-08 19:57:05 +0530

    c++

---
+++ c.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n1,n2,sum;
+    float avg;
+    cout<<"enter 1st no";
+    cin>>n1;
+    cout<<"enter 2nd no";
+    cin>>n2;
+sum=n1+n2;
+avg=sum/2;
+cout<<"sum of two no ="<<sum<<endl;
+cout<<"avg of two no ="<<avg<<endl;
+
+    return 0;
+}
